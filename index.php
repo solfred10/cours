1369fa7ba41acfec4d10aaa68299d0f19dd5bda9
solfred10 <sol.fred10@gmail.com>
2023-06-02 17:25:29 +0200

    MAJ page acceuil

---
+++ index.php
@@ -0,0 +1,94 @@
+
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">				
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<link href="css/style.css" rel="stylesheet">				
+		<title>Cours</title>
+        </style>
+	</head>
+	
+	<div class="container">
+		<div class="row MT20">
+			<div class="col-lg-12 header titre =" id="header">
+				 
+			</div>			
+		</div>
+		
+		<div class="row MT20">
+			<div class="col-lg-12 titre">
+				 <h1>Cours d'informatique par Frédéric SOL</h1>
+			</div>			
+		</div>		
+		
+				
+			<div class="row MT20">			
+				<div class="col-lg-3 titre">
+					<a href="cours/bootstrap/accueil.html">												
+						<img src="images/icones/iconeBoostrap.png" />
+					</a>	
+				</div>	
+				
+				<div class="col-lg-3 titre">
+					<a href="cours/css/accueil.html">
+						<img src="images/icones/iconeCSS.png" /> 
+					</a>
+				</div>		
+                
+                <div class="col-lg-3 titre">	
+                <a href="cours/html/accueil.html">												
+						<img src="images/icones/iconeHTML.png" /> 
+					</a>
+				</div>	
+			</div>	
+
+			<div class="row MT20" >
+				<div class="col-lg-3 titre">
+                    <a href="cours/javaScript/accueil.html">	
+					    <img src="images/icones/iconeJavascript.png" /> 
+                    </a> 
+				</div>	
+
+				<div class="col-lg-3 titre">		
+					<a href="cours/jquery/accueil.html">
+					<img src="images/icones/iconeJquery.png" /> 
+					</a>
+				</div>
+				
+				<div class="col-lg-3 titre">
+					<a href="cours/jqueryui/accueil.html">
+						<img src="images/icones/iconeJqueryUI.png" /> 
+					</a>	
+				</div>			
+			</div>		
+				
+			<div class="row MT20 MB50">
+				<div class="col-lg-3 titre">
+                    <a href="cours/php/accueil.html">												
+					    <img src="images/icones/iconePHP.png" /> 
+                    </a>    
+				</div>	
+
+				<div class="col-lg-3 titre">
+					<img src="images/icones/iconeSQL.png" /> 
+				</div>
+				
+				<div class="col-lg-3 titre">
+					<img src="images/icones/iconeWordPress.png" /> 
+				</div>			
+			</div>
+			</div>
+	
+	<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+	<!-- On charge Jquery en premier puis Popper (utilse pour Boostrap) puis la librairie Jquery propre à Boostrap 
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>-->	
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	
+	<!-- On charge le fichier JS contenant les fonctions utiles pour tout le site -->
+    <!-- <script src="js/fonctionsJS.js"></script> -->
+
+</html>
